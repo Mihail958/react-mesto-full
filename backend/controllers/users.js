100794f0a7c8ba8ac9ed097c301bf5902d7f58fc
Mihail <misha95814@gmail.com>
2022-09-04 13:15:45 +0300

    15.2.4

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -122,9 +122,10 @@ module.exports.updateAvatar = (req, res, next) => {
 
 module.exports.getCurrentUser = (req, res, next) => {
   User.findById(req.user._id)
+    // eslint-disable-next-line consistent-return
     .then((user) => {
       if (!user._id) {
-        next(new NotFound('Пользователь не найден'));
+        return next(new NotFound('Пользователь не найден'));
       }
       res.status(200).send(user);
     })
